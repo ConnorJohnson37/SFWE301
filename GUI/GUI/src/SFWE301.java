f7eedf639635fc0e1df7c52a2f601d865298e853
mvillasana <mvillasana@email.arizona.edu>
2023-11-24 13:28:31 -0700

    michaels GUI stuff

---
+++ GUI/GUI/src/SFWE301.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sfwe301;
+
+
+public class SFWE301 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        UploadGUI gui = new UploadGUI();
+        //ReviewGUI gui = new ReviewGUI();
+        //FurtherReviewGUI gui = new FurtherReviewGUI();
+        
+    }
+    
+}
